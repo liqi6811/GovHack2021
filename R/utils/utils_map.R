1f37f1fea5f43c9d4877c84e52c66e6d23f68db8
Ramee <ramee.elshabasei@mutiny.group>
2021-08-21 14:44:24 +1000

    Add simple R function to get SA1 from address

---
+++ R/utils/utils_map.R
@@ -0,0 +1,15 @@
+library(tidygeocoder)
+library(sf)
+
+sa1s = st_read("data/sa1/SA1_2021_AUST_GDA2020.shp")
+
+address_to_sa1 = function(address) {
+    out = geo(address=address, method="osm")
+    x = c(out$long, out$lat)
+
+    point = st_sfc(st_point(x), crs=7844)
+    idx = st_intersects(point, sa1s)
+    sa1s[idx[[1]],]$SA1_CODE21
+}
+
+address_to_sa1("361 Mount Dandenong Tourist Rd, Sassafras")
