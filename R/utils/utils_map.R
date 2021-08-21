349792effcea249fedc47f749a854ba62a45b1b9
Ramee <ramee.elshabasei@mutiny.group>
2021-08-21 15:27:43 +1000

    Filter to VIC only

--- R/utils/utils_map.R
+++ R/utils/utils_map.R
@@ -2,6 +2,7 @@ library(tidygeocoder)
 library(sf)
 
 sa1s = st_read("data/sa1/SA1_2021_AUST_GDA2020.shp")
+sa1s = sa1s[sa1s$STE_NAME21 == "Victoria",]
 
 address_to_sa1 = function(address) {
     out = geo(address=address, method="osm")
