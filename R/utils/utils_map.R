743381bfb9660fdd5762ed118c63485b3b97abb5
Ramee <ramee.elshabasei@mutiny.group>
2021-08-22 16:10:46 +1000

    Update to latest pricing code

--- R/utils/utils_map.R
+++ R/utils/utils_map.R
@@ -1,8 +1,10 @@
 library(tidygeocoder)
+library(data.table)
 library(sf)
 
 sa1s = st_read("../data/vic_sa1/MB_2016_VIC.shp")
 sa1s = sa1s[sa1s$STE_NAME16 == "Victoria",]
+print(head(sa1s))
 
 address_to_coords = function(address) {
     out = geo(address=address, method="osm")
@@ -10,10 +12,14 @@ address_to_coords = function(address) {
 }
 
 coords_to_sa1 = function(lat, lon) {
+    if (is.na(lat) || is.na(lon))
+        return (NA)
     x = c(lon, lat)
     point = st_sfc(st_point(x), crs=4283)
     idx = st_intersects(point, sa1s)
-    sa1s[idx[[1]],]
+    print(typeof(sa1s))
+    print(sa1s[idx[[1]],])
+    as.data.table(sa1s[idx[[1]],])
 }
 
 address_to_sa1 = function(address) {
@@ -22,6 +28,7 @@ address_to_sa1 = function(address) {
 
     point = st_sfc(st_point(x), crs=4283)
     idx = st_intersects(point, sa1s)
+    print(sa1s[idx[[1]],])
     sa1s[idx[[1]],]
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # GovHack2021
 2021 GovHack
+
+To run the RShiny dashboard, open an R console and type
+```
+library(shiny)
+runApp("R")
+```
+You will need to make sure all the dependencies are available:
+- sf
+- dplyr
+- shinythemes
+- tidyverse
+- data.table
+- ggplot2
+- magrittr
